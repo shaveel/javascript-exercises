525964ac3d2853e38356df66f8aa4b92f1d3128c
Shaveel <shaveel.prasad17@gmail.com>
2023-03-09 16:03:18 +1100

    Complete

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year < 0) {
+        return "ERROR"
+    }
+    else if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true
+            }
+            return false
+        }
+        return true
+    }
+    
+    return false
 };
 
 // Do not edit below this line
