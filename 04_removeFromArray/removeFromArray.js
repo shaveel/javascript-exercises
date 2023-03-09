886b1cebda118dfb06193f6a52b030cf300b0f92
Shaveel <shaveel.prasad17@gmail.com>
2023-03-09 15:14:53 +1100

    Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let index;
+    for (i = 1; i < arguments.length; i++) {
+        index = array.indexOf(arguments[i]);
+        if (!(index < 0)) {
+            array.splice(index,1);
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
