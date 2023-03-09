476931658e9034a4a9f3cbdc7bbb0fea1cc7fb81
Shaveel <shaveel.prasad17@gmail.com>
2023-03-09 15:48:49 +1100

    Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (typeof(num1) !== "number" || typeof(num2) !== "number" || num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+    let min = Math.min(num1, num2);
+    let max = Math.max(num1, num2);
+    let result = 0;
+
+    for (i = 0; i < (max - min)+1;i++) {
+        result += (min + i);
+    }
+
+    return result
 };
 
 // Do not edit below this line
