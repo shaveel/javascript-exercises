114b247cd0cbfb261e852dae56fd9f56f88c8a0c
Shaveel <shaveel.prasad17@gmail.com>
2023-03-09 13:05:12 +1100

    Complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let result = "";
+    for (i = 0; i < string.length + 1; i++) {
+        result = result + string.substr(string.length - i, 1);
+    }
+    return result
 };
 
 // Do not edit below this line
